e55b86c8973f131ef6826afc63b2d7776ce06825
Angela Damaso <angela.damaso@kaplan.com>
2018-04-26 10:21:24 -0400

    variables exercise

---
+++ prac-var.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declare the following variables with zero values:
+	// name: command, type string
+	// name: valid, type bool
+	
+	var command string
+	var valid bool
+
+	// print the value of those variables
+	
+	fmt.Println(command)
+	fmt.Println(valid)
+
+	// declare and initialize the following variables:
+	// name: foo, type: int, value: 5
+	// name bar, type: bool, value: true
+	
+	foo := 5
+	bar := true
+
+	// print the value of those variables
+	
+	fmt.Println(foo)
+	fmt.Println(bar)
+
+}
